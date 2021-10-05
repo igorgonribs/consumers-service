2da95d3bdd5ae39e2270e2be8440b7e7f0daef36
igor <igorg.silva@bradesco.com.br>
2021-10-05 09:42:49 -0300

    Autenticação ok

--- src/main/java/br/com/igor/challenge/consumer/SecurityConfig.java
+++ src/main/java/br/com/igor/challenge/consumer/SecurityConfig.java
@@ -1,25 +1,34 @@
 package br.com.igor.challenge.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private AuthenticationEntryPoint authEntryPoint;
+	private static final String ROLE_CONSUMER = "CONSUMER";
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic()
-				.authenticationEntryPoint(authEntryPoint);
+		http.authorizeRequests().antMatchers("/api/**").hasRole(ROLE_CONSUMER).and().httpBasic();
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}password").roles("USER");
+		auth.inMemoryAuthentication()
+			.withUser("bankxpto").password(passwordEncoder().encode("bankxptopassword")).roles(ROLE_CONSUMER).and()
+			.withUser("appxpto").password(passwordEncoder().encode("appxptopassword")).roles(ROLE_CONSUMER).and()
+			.withUser("userxpto").password(passwordEncoder().encode("userxptopassword")).roles(ROLE_CONSUMER);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
