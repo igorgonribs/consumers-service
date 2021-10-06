440942b27dca31b3454f100ef8ac55698bbedf29
igor <igorg.silva@bradesco.com.br>
2021-10-06 10:43:57 -0300

    Criação de formatação para machine learning na consulta da base b

--- src/main/java/br/com/igor/challenge/consumer/controller/ClientController.java
+++ src/main/java/br/com/igor/challenge/consumer/controller/ClientController.java
@@ -1,5 +1,6 @@
 package br.com.igor.challenge.consumer.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.igor.challenge.consumer.entity.ClientDebts;
 import br.com.igor.challenge.consumer.entity.ClientGoods;
+import br.com.igor.challenge.consumer.entity.Good;
 import br.com.igor.challenge.consumer.repository.ClientDebtsRepository;
 import br.com.igor.challenge.consumer.repository.ClientGoodsRepository;
 
@@ -34,6 +36,25 @@ public class ClientController {
 	public ResponseEntity<?> getGoods(@PathVariable("cpf") String cpf) {
 		Optional<ClientGoods> clientGoods = goodsRepository.findByCpf(cpf);
 		return resolveResponse(clientGoods);
+	}
+	
+	@GetMapping("/goods/machine-learning/{cpf}")
+	public ResponseEntity<?> getGoodsForMachineLearning(@PathVariable("cpf") String cpf) {
+		Optional<ClientGoods> clientGoods = goodsRepository.findByCpf(cpf);
+		
+		formatToMachineLearning(clientGoods);
+		return resolveResponse(clientGoods);
+	}
+
+	private void formatToMachineLearning(Optional<ClientGoods> clientGoods) {
+		if(clientGoods.isPresent()) {
+			clientGoods.get().setPatrimony(calculatePatrimony(clientGoods.get().getGoods()));
+			clientGoods.get().setGoods(null);
+		}
+	}
+
+	private double calculatePatrimony(List<Good> goods) {
+		return goods.stream().mapToDouble(good->good.getValue()).reduce(0.0, Double::sum);
 	}
 
 	private ResponseEntity<?> resolveResponse(Optional<?> item) {
--- src/main/java/br/com/igor/challenge/consumer/entity/ClientGoods.java
+++ src/main/java/br/com/igor/challenge/consumer/entity/ClientGoods.java
@@ -6,11 +6,16 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Data;
 
 @Entity
 @Data
+@JsonInclude(value = Include.NON_NULL)
 public class ClientGoods {
 
 	@Id
@@ -18,6 +23,9 @@ public class ClientGoods {
 	public int age;
 	public double income;
 	public String address;
+	
+	@Transient
+	public double patrimony;
 
 	@OneToMany(mappedBy = "clientGoods", cascade = CascadeType.ALL)
 	public List<Good> goods;
